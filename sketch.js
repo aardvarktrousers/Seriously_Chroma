5a225245d16a0c272e31af720ae2977d49eb49c9
aardvarktrousers <aardvarktrousers@yahoo.co.uk>
2019-03-07 23:04:53 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
-var capture;
+let capture;
 
 function setup() {
   createCanvas(480, 480);
   capture = createCapture(VIDEO);
-  capture.hide();
+  //capture.hide();
 }
 
 function draw() {
   image(capture, 0, 0, width, width * capture.height / capture.width);
-  filter(INVERT);
+  filter('INVERT');
 }
