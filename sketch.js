283a417903b11519bbaa557e8723fc79c630ec5f
aardvarktrousers <aardvarktrousers@yahoo.co.uk>
2019-03-08 18:57:24 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,34 @@
-var capture;
+var video;
+
 
 function setup() {
-  createCanvas(390, 240);
-  capture = createCapture(VIDEO);
-  capture.size(320, 240);
-  //capture.hide();
+  canvas = createCanvas(640, 480, WEBGL);
+  canvas.id('p5canvas');
+  canvas.position(0, 0);
+  
+  video = createCapture(VIDEO);
+  video.elt.setAttribute('playsinline', '');
+  video.size(640, 480);
+  video.id('p5video');
+  video.hide();
+  
+  var seriously = new Seriously();
+  var src = seriously.source('#p5video');
+  var target = seriously.target('#p5canvas');
+  target.width = 640;
+  target.height = 480;
+  
+var chroma = seriously.effect('chroma');
+chroma.source = "#p5video";
+target.source = chroma;
+  var r = 98 /255;
+  var g = 175 / 255;
+  var b = 116 / 255;
+  chroma.screen = [r, g, b, 1];
+  
+  seriously.go();
 }
 
 function draw() {
-  background(255);
-  image(capture, 0, 0, 320, 240);
-  filter('INVERT');
+  
 }
