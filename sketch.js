3d4f672d0cd94ea16a2c5333b7dea7ed2e08db75
aardvarktrousers <aardvarktrousers@yahoo.co.uk>
2019-03-08 09:24:38 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,54 +1,14 @@
-/*
- * @name Geometries
- * @description There are six 3D primitives in p5 now.
- */
-function setup(){
-  createCanvas(710, 400, WEBGL);
+let capture;
+
+function setup() {
+  createCanvas(390, 240);
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  //capture.hide();
 }
 
-function draw(){
-  background(250);
-  translate(-250 * 2.5, 0, 0);
-  normalMaterial();
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  plane(80);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  box(80, 80, 80);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  cylinder(80, 80);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  cone(80, 80);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  torus(80, 20);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * 0.01);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  sphere(80);
-  pop();
+function draw() {
+  background(255);
+  image(capture, 0, 0, 320, 240);
+  filter('INVERT');
 }
